bd8c8399bd4c2195833260dec261cd3c1e7ae34c
Indigo <j.c.f.gauss@sjtu.edu.cn>
2021-11-04 12:59:27 +0800

    avoid function

--- src/PRBase.jl
+++ src/PRBase.jl
@@ -14,9 +14,7 @@ zro = Zero()
 
 struct Proj{T} <: AbstractPrimRec end
 
-function (::Proj{T})(xs...) where {T}
-    return xs[T]
-end
+(::Proj{T})(xs...) where {T} = xs[T]
 
 struct Comb <: AbstractPrimRec
     f::AbstractPrimRec
@@ -24,21 +22,19 @@ struct Comb <: AbstractPrimRec
     Comb(f, gs...) = new(f, gs)
 end
 
-function (c::Comb)(xs...)
-    return c.f((g(xs...) for g in c.gs)...)
-end
+(c::Comb)(xs...) = c.f((g(xs...) for g in c.gs)...)
 
 struct PrimRec <: AbstractPrimRec
     basic::AbstractPrimRec
     step::AbstractPrimRec
 end
 
-function (pr::PrimRec)(xs...)
+(pr::PrimRec)(xs...) = begin
     t = xs[1]
     y = xs[2:end]
     cur = pr.basic(y...)
     for i in 0:(t - 1)
         cur = pr.step(cur, i, y...)
     end
-    return cur
+    cur
 end
